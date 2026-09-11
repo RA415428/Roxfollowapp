a3aa14dd902bfa49e216a9e7c27b535afc185fbc
RA415428 <nayakhardayal4@gmail.com>
2026-09-11 08:58:56 +0530

    Fix missing closing div in CoinsScreen watch ad button

--- src/screens/CoinsScreen.tsx
+++ src/screens/CoinsScreen.tsx
@@ -287,6 +287,7 @@ export const CoinsScreen: React.FC<CoinsScreenProps> = ({
               </>
             )}
           </button>
+          </div>
         </div>
 
         {/* Status subrow */}
